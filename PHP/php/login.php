4da4827da7b45a2fe023756c4b257b9c0f969683
朝飞暮卷 <1218304973@qq.com>
2021-11-05 05:34:46 +0000

    服务端登录文件 sql语句用count=1判断可否登录

---
+++ PHP/php/login.php
@@ -0,0 +1,28 @@
+<?php
+$user = isset($_POST['user']) ? htmlspecialchars($_POST['user']) : '';
+$passwd = isset($_POST['passwd']) ? htmlspecialchars($_POST['passwd']) : '';
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "htzy0000";
+// 创建连接
+$con = new mysqli($servername, $username, $password);
+
+// 选择数据库
+mysqli_select_db($con,"bysj");
+// 设置编码，防止中文乱码
+mysqli_set_charset($con, "utf8");
+ 
+$stmt = $con->prepare("SELECT count(*) as login_status FROM bysj.user WHERE username = ? and passwd = ?");
+$stmt->bind_param("ss",$user,$passwd);
+$stmt->bind_result($login_status);
+$stmt->execute();
+
+while($stmt->fetch()){
+ echo $login_status;
+}
+
+$stmt->free_result();
+$stmt->close();
+mysqli_close($con);
+?>
