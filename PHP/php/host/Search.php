8275ea1ccb9e5b467d4de059543207d1eec36cf6
wxk <1218304973@qq.com>
2021-11-29 18:09:00 +0800

    增加： 1，设备列表 搜索接口

---
+++ PHP/php/host/Search.php
@@ -0,0 +1,32 @@
+<?php
+$search = isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '';
+$con = null;
+$id  = null;
+$host_name = null;
+$host_type = null;
+$host_ip = null;
+$cpu_model  = null;
+$cpu_core = null;
+$mem_total = null;
+$swap_total = null;
+$network_model = null;
+$network_speed = null;
+$network_num = null;
+$disk_num = null;
+$disk_all = null;
+require_once "../linkDB.php";
+mysqli_select_db($con,"bysj");
+// 设置编码，防止中文乱码
+mysqli_set_charset($con, "utf8");
+//利用数据行数判定登录
+$stmt = $con->prepare("select id,host_name,host_type,host_ip,mem_total,network_speed,disk_all from bysj.host where host_name = ? or host_type = ? or host_ip = ?");
+$stmt->bind_param("sss",$search,$search,$search);
+$stmt->bind_result($id,$host_name,$host_type,$host_ip,$mem_total,$network_speed,$disk_all);
+$stmt->execute();
+while($stmt->fetch()){
+    echo "
+    <tr>
+        <td>$host_name</td><td>$host_type</td><td>$host_ip</td><td>$mem_total MB</td><td>$network_speed</td><td>$disk_all</td><td id='host_$id' class='deleteBtn' onclick='deleteHost(this)'>删除</td>
+    </tr>";
+}
+?>
