fe333c723f50ef990993987adf503776f7f9c36b
wxk <1218304973@qq.com>
2021-12-21 17:14:57 +0800

    时间回溯 3

--- js/panel/disk.js
+++ js/panel/disk.js
@@ -3,9 +3,9 @@ var xmlHttpDiskWrite;
 var xmlHttpDiskRead;
 
 function runDisk(){
-    xmlHttpDiskTime = pgGet("http://127.0.0.1/php/panel/disk.php?type=datatime&username="+getCookie("UserName")+"&token="+getCookie("Token"),diskback);
-    xmlHttpDiskWrite = pgGet("http://127.0.0.1/php/panel/disk.php?type=diskwrite&username="+getCookie("UserName")+"&token="+getCookie("Token"),diskback);
-    xmlHttpDiskRead = pgGet("http://127.0.0.1/php/panel/disk.php?type=diskread&username="+getCookie("UserName")+"&token="+getCookie("Token"),diskback);
+    xmlHttpDiskTime = pgGet("http://192.168.157.128/php/panel/disk.php?type=datatime&username="+getCookie("UserName")+"&token="+getCookie("Token"),diskback);
+    xmlHttpDiskWrite = pgGet("http://192.168.157.128/php/panel/disk.php?type=diskwrite&username="+getCookie("UserName")+"&token="+getCookie("Token"),diskback);
+    xmlHttpDiskRead = pgGet("http://192.168.157.128/php/panel/disk.php?type=diskread&username="+getCookie("UserName")+"&token="+getCookie("Token"),diskback);
 }
 function diskback(){
     viewCharts('disk',"写速度","读速度","KB/s");
