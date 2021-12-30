438ce7a298789d039eece748b953d4233a17c315
wxk <1218304973@qq.com>
2021-12-30 13:13:06 +0800

    尝试WebShell

--- Server/WebShell/shell.php
+++ Server/WebShell/shell.php
@@ -1,93 +1,128 @@
 <?php
+//问题1：数据库不能远程连接 :: /etc/mysql/mysql.conf.d/mysql.cnf >> #bind-address = 127.0.0.1
+$servername = '192.168.157.128';
+$username = 'root';
+$password = 'Esxi0000.';
 
-/**
- * websocket服务器（使用swoole）
- * 使用ssh登录服务器
- */
-class Ws
-{
-    private $shell;
-    private $connection;
-    private $isConnection;
+// 创建连接
+$con = new mysqli($servername, $username, $password);
+// 选择数据库
+mysqli_select_db($con, 'bysj');
+// 设置编码，防止中文乱码
+mysqli_set_charset($con, 'utf8');
 
-    private $ws;
+//设置无限请求超时时间
+set_time_limit(0);
 
-    public function __construct()
-    {
-        //监听0.0.0.0:8081端口
-        $this->ws = new Swoole\WebSocket\Server('0.0.0.0', 8081);
+$ip = `ip a | grep inet | sed -n '3p' | awk -F ' ' '{print $2}' | awk -F '/' '{print $1}'`;
+$port = 1094;
 
-        $this->ws->on('open', [$this, 'onOpen']);
-        $this->ws->on('message', [$this, 'onMessage']);
-        $this->ws->on('close', [$this, 'onClose']);
-
-        $this->ws->start();
-    }
-
-    //监听WebSocket连接打开事件
-    public function onOpen($ws, $request)
-    {
-        var_dump($request->fd, $request->server);
-    }
-
-    //监听WebSocket消息事件
-    public function onMessage($ws, $frame)
-    {
-        $data = json_decode($frame->data, true);
-        switch (key($data)) {
-            case 'data':    //输入命令
-                fwrite($this->shell[$frame->fd], $data['data']);
-                usleep(800);
-                while ($line = fgets($this->shell[$frame->fd])) {
-                    $ws->push($frame->fd, $line);
-                }
-                break;
-            case 'auth':    //登录
-                if ($this->loginSSH($data['auth'], $frame)) {
-                    $ws->push($frame->fd, '连接中...');
-                    while ($line = fgets($this->shell[$frame->fd])) {
-                        $ws->push($frame->fd, $line);
-                    }
-                } else {
-                    $ws->push($frame->fd, '登录失败');
-                }
-                break;
-            default:
-                //清理空白行
-                if ($this->isConnection[$frame->fd]) {
-                    while ($line = fgets($this->shell[$frame->fd])) {
-                        $ws->push($frame->fd, $line);
-                    }
-                }
-                break;
-        }
-    }
-
-    //监听WebSocket连接关闭事件
-    public function onClose($ws, $fd)
-    {
-        $this->isConnection[$fd] = false;
-        echo "client-{$fd} is closed\n";
-    }
-
-    //ssh登录
-    public function loginSSH($auth, $frame)
-    {
-        //通过SSH连接服务器
-        $this->connection[$frame->fd] = ssh2_connect($auth['server'], $auth['port']);
-        //验证身份（登录）
-        if (ssh2_auth_password($this->connection[$frame->fd], $auth['user'], $auth['password'])) {
-            //使用流的方式打开shell
-            $this->shell[$frame->fd] = ssh2_shell($this->connection[$frame->fd], 'xterm', null, 80, 24, SSH2_TERM_UNIT_CHARS);
-            sleep(1);   //延迟执行一秒等待服务器
-            $this->isConnection[$frame->fd] = true;
-            return true;
-        } else {
-            return false;
-        }
-    }
+//创建socket
+if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) < 0) {
+//    echo "socket_create() 失败的原因是:".socket_strerror($sock)."\n";
+    exit();
+}
+//把socket绑定在一个IP地址和端口上
+if (($ret = socket_bind($sock, $ip, $port)) < 0) {
+//    echo "socket_bind() 失败的原因是:".socket_strerror($ret)."\n";
+    exit();
+}
+//监听由指定socket的所有连接
+if (($ret = socket_listen($sock, 4)) < 0) {
+//    echo "socket_listen() 失败的原因是:".socket_strerror($ret)."\n";
+    exit();
 }
 
-new ws();
+do {
+    //接收一个Socket连接
+    if (($msgsock = socket_accept($sock)) < 0) {
+//        echo "socket_accept() failed: reason: " . socket_strerror($msgsock) . "\n";
+        break;
+    } else {
+        $buf = socket_read($msgsock, 2048);
+//        echo $buf."\n";
+        //从第0个字符,往后取4个字符
+        //消息参数 ping:ping177  cpus:cpus disk:disk nets:nets
+        $go = substr($buf, 0, 4);
+//        echo $go."\n";
+
+        switch ($go) {
+            case 'ping':
+                $hostIP = null;
+                //从第四个字符取到最后
+                $hostID = substr($buf, 4);
+//                echo $hostID . "\n";
+                $stmt = $con->prepare('select host_ip from bysj.host where id = ?');
+                $stmt->bind_param('s', $hostID);
+                $stmt->bind_result($hostIP);
+                $stmt->execute();
+                while ($stmt->fetch()) {
+                    $msg = `jaina ping $hostIP`;
+                    //$msg = <img src='/img/go.png' width='12%'> or <img src='/img/stop.png' width='12%'>
+//                    echo "发送:" . $msg;
+                    socket_write($msgsock, $msg, strlen($msg));
+                }
+                break;
+            case 'cpus':
+                $msg = `jaina testcpus`;
+                //$msg = 10:42:18 up 2 days, 3:14, 3 users, load average: 0.04, 0.05, 0.01
+//                echo "发送:" . $msg;
+                socket_write($msgsock, $msg, strlen($msg));
+                break;
+            case 'disk':
+                $msg = `jaina testdisk`;
+                //$msg = 写入速度：1.0GB/s 读取速度：7.5GB/s
+//                echo "发送:" . $msg;
+                socket_write($msgsock, $msg, strlen($msg));
+                break;
+            case 'nets':
+                $msg = `jaina testnets`;
+                //$msg = Hosted by China Telecom JiangSu 5G (Suzhou) [118.30 km]: 14.205 ms Download: 94.05 Mbit/s Upload: 40.35 Mbit/s
+//                echo "发送:" . $msg;
+                socket_write($msgsock, $msg, strlen($msg));
+                break;
+            case 'port':
+                $msg = `jaina testport`;
+                //$msg = <tr> <td>ubuntu</td> <td>127.0.0.1</td> <td>tcp</td> <td>58468 </td> <td>2675841/php </td> </tr>
+//                echo "发送:" . $msg;
+                socket_write($msgsock, $msg, strlen($msg));
+                break;
+            case 'cpui':
+                $msg = `jaina testcpui`;
+                //$msg = <td>2</td> <td> AMD Ryzen 5 2500U with Radeon Vega Mobile Gfx</td> <td> 2000.000 GHz</td>
+//                echo "发送:" . $msg;
+                socket_write($msgsock, $msg, strlen($msg));
+                break;
+            case 'moth':
+                $msg = `jaina testmoth`;
+                //$msg = <td> 440BX Desktop Reference Platform</td>
+//                echo "发送:" . $msg;
+                socket_write($msgsock, $msg, strlen($msg));
+                break;
+            case 'memo':
+                $msg = `jaina testmemo`;
+                //$msg = <td>1</td> <td> Size: 2048 MB</td>
+//                echo "发送:" . $msg;
+                socket_write($msgsock, $msg, strlen($msg));
+                break;
+            case 'neti':
+                $msg = `jaina testneti`;
+                //$msg = <td>02:01.0 Ethernet controller: Intel Corporation 82545EM Gigabit Ethernet Controller (Copper) (rev 01)</td> <td>'1000Mb/s</td>
+//                echo "发送:" . $msg;
+                socket_write($msgsock, $msg, strlen($msg));
+                break;
+            case 'diki':
+                $msg = `jaina testdiki`;
+                //$msg = <td>1</td> <td>00:10.0 SCSI storage controller: Broadcom / LSI 53c1030 PCI-X Fusion-MPT Dual Ultra320 SCSI (rev 01)</td> <td>50G</td>
+//                echo "发送:" . $msg;
+                socket_write($msgsock, $msg, strlen($msg));
+                break;
+            default:
+                break;
+        }
+    }
+    //关闭socket
+    socket_close($msgsock);
+} while (true);
 
 ?>
