907b231274889b79c9ce3d5e91a3bf4a1bd0b1bb
wxk <1218304973@qq.com>
2022-01-06 17:36:39 +0800

    修复 登录标识符显示问题 && 完善 shell退出操作 && 掩盖 第一条命令不回显+标识符缺失问题

--- Server/WebShell/shell.php
+++ Server/WebShell/shell.php
@@ -39,13 +39,12 @@ do {
         $ip = explode("+",$buf)[0];
         $cmd = explode("+",$buf)[1];
         echo "命令：".$cmd."\n";
-        if ($buf == 'exit') {
-            exit();
-        }
+
         $user= explode("+",$buf)[2];
         $pass = explode("+",$buf)[3];
 
-        //未连接的IP == 数组为空
+        //未连接的IP == 数组为空(第一次连接时会走这儿)
+        //BUG:连接成功后，不执行命令就会一直呆在这儿
         if ( !in_array($ip,$shell_ip) || empty($shell_ip) ){
             //ip进栈
             array_push($shell_ip,$ip);
@@ -53,20 +52,28 @@ do {
             $shell[array_search($ip,$shell_ip)] = ssh2_connect(end($shell_ip), 22);
             ssh2_auth_password($shell[array_search($ip,$shell_ip)], $user, $pass);
             $shell_link[array_search($ip,$shell_ip)] = ssh2_shell($shell[array_search($ip,$shell_ip)], 'vt102', null, 80, 24, SSH2_TERM_UNIT_CHARS);
+            socket_write($msgsock, '连接成功2022年1月6日', strlen('连接成功2022年1月6日'));
         }
 
-        fwrite($shell_link[array_search($ip,$shell_ip)], $cmd);
-
-        sleep(1);
-        //发送命令结果
-       while ($line = fgets($shell_link[array_search($ip,$shell_ip)])) {
-               if ((strpos($line,'@')&&strpos($line,'~#')) || (strpos($line,'@')&&strpos($line,'$'))){
-                   socket_write($msgsock, "der".$line."der", strlen('der'.$line .'der'));
-               }else{
-                   socket_write($msgsock, $line, strlen($line));
-               }
-               echo '返回结果：' . $line;
-       }
+        //命令是“exit+回车”
+        if ( strncmp("exit",$cmd,4)==0 && strlen($cmd)==5 ) {
+            $shell[array_search($ip,$shell_ip)] = null;
+            $shell_link[array_search($ip,$shell_ip)] = null;
+            $shell_ip[array_search($ip,$shell_ip)] = null;
+            echo $ip."已退出!";
+        }else{
+            fwrite($shell_link[array_search($ip, $shell_ip)], $cmd);
+            sleep(1);
+            //发送命令结果
+            while ($line = fgets($shell_link[array_search($ip, $shell_ip)])) {
+                if ((strpos($line, '@') && strpos($line, '~#')) || (strpos($line, '@') && strpos($line, '$'))) {
+                    socket_write($msgsock, 'der' . $line . 'der', strlen('der' . $line . 'der'));
+                } else {
+                    socket_write($msgsock, $line, strlen($line));
+                }
+                echo '返回结果：' . $line;
+            }
+        }
     }
     //关闭socket
     socket_close($msgsock);
